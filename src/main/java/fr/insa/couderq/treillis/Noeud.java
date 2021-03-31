ea225446ab34ceddcb9793834035323f012018c5
Administrateur <Administrateur@172.29.5.131>
2021-03-31 09:32:06 +0200

    init

---
+++ src/main/java/fr/insa/couderq/treillis/Noeud.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.couderq.treillis;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Noeud {
+    
+    private int id;
+    private double px;
+    private double py;
+    
+    public Noeud (double x, double y){
+        int k=0;
+        this.id = k;
+        this.px = x;
+        this.py = y;
+        k=k+1;
+    }//pbm avec id
+    
+    public 
+}
